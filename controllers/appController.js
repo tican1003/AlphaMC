f58528d38363ed6acd04d3eb88da44bc4918a813
tican1003 <tricuong.can@gmail.com>
2022-08-01 10:32:04 +0700

    modified:   Templates/index.dwt 	new file:   assets/img/hus0001.jpg 	new file:   assets/img/hus0002.jpg 	modified:   assets/img/hus0003.jpg 	new file:   assets/js/index.js 	modified:   controllers/appController.js 	modified:   index.html 	new file:   public/responsive.css 	modified:   public/style.css 	modified:   rules.html 	modified:   support.html

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,18 +1,5 @@
+// const path = require('path');
+
 exports.getHome = async (req, res) => {
-  // ERROR
-  res.sendFile('../index.html', { root: __dirname });
-  //   try {
-  //     const data = await
-  //     res.status(200).json({
-  //         status: 'success',
-  //         data: {
-  //             res.
-  //         }
-  //     })
-  //   } catch (err) {
-  //     res.status(400).json({
-  //       status: 'fail',
-  //       message: err,
-  //     });
-  //   }
+  res.redirect('/index.html');
 };
